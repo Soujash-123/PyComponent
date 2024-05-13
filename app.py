43a2c65536e07d5ffefe6c5efa5f0513fb6e160b
Soujash-123 <72877997+Soujash-123@users.noreply.github.com>
2024-05-13 20:06:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
 from flask import Flask, request, jsonify
-import ast
+from PyComponent import analyze_python_code
 
 app = Flask(__name__)
 
-def detect_scopes(code):
-    tree = ast.parse(code)
-    scopes = []
-
-    def extract_scopes(node, parent_scope=None):
-        if isinstance(node, ast.FunctionDef):
-            scope_type = "Function"
-            scope_name = node.name
-            scope_contents = ast.get_source_segment(code, node)
-            scopes.append({"Type": scope_type, "Name": scope_name, "Contents": scope_contents})
-        elif isinstance(node, ast.ClassDef):
-            scope_type = "Class"
-            scope_name = node.name
-            inner_scopes = []
-            for child_node in node.body:
-                extract_scopes(child_node, parent_scope=scope_name)
-            scope_contents = [scope["Name"] for scope in inner_scopes]
-            scopes.append({"Type": scope_type, "Name": scope_name, "Contents": scope_contents})
-            return
-        for child_node in ast.iter_child_nodes(node):
-            extract_scopes(child_node, parent_scope=parent_scope)
-
-    extract_scopes(tree)
-
-    return scopes
-
-@app.route('/detect_scopes', methods=['POST'])
-def handle_detect_scopes():
+@app.route('/analyze', methods=['POST'])
+def analyze():
     data = request.get_json()
-    code = data.get('code', '')
-    if code:
-        scopes = detect_scopes(code)
-        return jsonify({"scopes": scopes})
-    else:
-        return jsonify({"error": "No code provided."}), 400
+    if 'code' not in data:
+        return jsonify({'error': 'Please provide code to analyze'}), 400
+    
+    code = data['code']
+    result = analyze_python_code(code)
+    
+    return result, 200
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
